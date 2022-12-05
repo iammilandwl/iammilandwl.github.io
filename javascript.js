853b439b609843b8b17704407362684b7623508c
Milan Dewaele <realmilandewaele@vivaldi.net>
2022-12-05 11:44:14 +0100

    changes

--- javascript.js
+++ javascript.js
@@ -3,4 +3,28 @@ var ads = ["https://i.imgur.com/mZtTGFB.gif", "https://i.imgur.com/oAxdc1r.gif",
 var ad = ads[Math.floor(Math.random() * ads.length)];
 
 $('#ad').attr('src', ad);
-$('#day').text(new Date().toLocaleString('en-us', {weekday:'long'}))
+$('#day').text(new Date().toLocaleString('en-us', {
+    weekday: 'long'
+}))
+
+function loadPage(page) {
+    ad = ads[Math.floor(Math.random() * ads.length)];
+    $('#ad').attr('src', ad);
+    $('.innerpage').removeClass('loaded');
+    setTimeout(() => {
+        $.ajax({
+            url: `/pages/${page}.html`,
+            success: function (data) {
+                $('.innerpage').html(data);
+                $('.innerpage').addClass('loaded');
+            },
+            error: function () {
+                $('.innerpage').html(`<h1>404</h1><p>Page not found</p>`);
+                $('.innerpage').addClass('loaded');
+            }
+
+        });
+    }, 500);
+
+
+}
